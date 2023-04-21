e66aaa4371f99995968dda0d10575eca35de50e0
jensenemi <jeemi@ucsd.edu>
2023-04-21 11:36:16 -0700

    commit

--- ArrayTests.java
+++ ArrayTests.java
@@ -28,4 +28,10 @@ public class ArrayTests {
     int[] input1 = {1, 2, 3, 4, 5};
     assertArrayEquals(new int[]{5, 4, 3, 2, 1}, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void negativesAverageWithoutLowest() {
+    double[] nums = {-2, -5, -3, 2, 9, 2};
+    assertEquals((Double)1.6, (Double)ArrayExamples.averageWithoutLowest(nums));
+  }
 }
